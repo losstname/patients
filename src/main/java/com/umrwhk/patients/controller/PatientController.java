9cab870b76802ce8aab5937f58f7ce9363f49cd3
Umar Wirahadikusuma <umrwhk@gmail.com>
2023-11-22 14:55:02 +0800

    chore: allow cross origin

--- src/main/java/com/umrwhk/patients/controller/PatientController.java
+++ src/main/java/com/umrwhk/patients/controller/PatientController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/patient")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class PatientController {
 
     private final PatientService patientService;
